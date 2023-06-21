08155b805998e7291809f337d718c8f91ad77056
mcuong174 <113322409+mcuong174@users.noreply.github.com>
2023-06-21 15:57:19 +0700

    Update NewTask.jsx

--- client/src/components/task/NewTask.jsx
+++ client/src/components/task/NewTask.jsx
@@ -14,7 +14,7 @@ const initialState = {
 
 export default function NewTask({ setNewTask, newTask }) {
   const axiosInstance = axios.create({
-    baseURL: "http://task-manager-server-liart.vercel.app/api/v1/posts",
+    baseURL: "https://servertasks.onrender.com/api/v1/posts",
   });
 
   const [state, setState] = useState(initialState);
