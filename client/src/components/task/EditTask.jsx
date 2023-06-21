39b69ba49e0ba9931a272f0384de5fe3ed654681
mcuong174 <113322409+mcuong174@users.noreply.github.com>
2023-06-21 17:48:13 +0700

    Update EditTask.jsx

--- client/src/components/task/EditTask.jsx
+++ client/src/components/task/EditTask.jsx
@@ -10,7 +10,7 @@ import "./EditTaskStyle.scss";
 
 export default function TaskItem() {
   const axiosInstance = axios.create({
-    baseURL: "https://servertasks.onrender.com/api/v1/posts",
+    baseURL: process.env.REACT_APP_API_URL_POSTS,
   });
 
   const [loading, setLoading] = useState(true);
