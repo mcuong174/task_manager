7a0bae942fc519956f19d7d2bc05d87de8c17e72
mcuong174 <113322409+mcuong174@users.noreply.github.com>
2023-06-21 09:46:31 +0700

    Update posts.router.js

--- server/routes/posts.router.js
+++ server/routes/posts.router.js
@@ -6,7 +6,7 @@ import postsController from "../controller/posts.controller.js";
 router.get("/tasks", postsController.getAll);
 router.get("/task/:id", postsController.getById);
 router.post("/newTask", postsController.create);
-router.put("/task/:id", postsController.update);
+router.patch("/task/:id", postsController.update);
 router.delete("/delete/task/:id", postsController.delete);
 
 export default router;
