2f28945db2d041f850abbedba455a684df6476d6
mcuong174 <113322409+mcuong174@users.noreply.github.com>
2023-06-21 10:04:21 +0700

    Update Register.jsx

--- client/src/components/pages/Register.jsx
+++ client/src/components/pages/Register.jsx
@@ -11,7 +11,7 @@ import "./RegisterStyle.scss";
 
 export default function Register() {
   const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL_AUTH,
+    baseURL: "http://task-manager-server-liart.vercel.app/api/v1/auth"
   });
 
   const navigate = useNavigate();
